0b210d7df8d803d17b3107accc234da0888bf83f
patnir <rahul20patni@gmail.com>
2016-06-23 12:46:02 -0400

    Validation on dates input

--- Logic.js
+++ Logic.js
@@ -5,6 +5,8 @@ function body_load() {
     btnAddNewEntry.onmousedown = btnAddNewEntry_onmousedown;
     btnBack.onmousedown = btnBack_onmousedown;
     btnRefresh.onmousedown = btnRefresh_onmousedown;
+    btnErrorMessageOK.onmousedown = btnErrorMessageOK_onmousedown;
+    errorMessageMain.style.visibility = 'hidden';
     btnAddNewEntry.style.visibility = "visible";
     btnBack.style.visibility = "hidden";
     window_onresize();
@@ -24,6 +26,11 @@ function window_onresize() {
     txtEndDate.style.width = (window.innerWidth - 210).toString() + "px";
 
     btnRefresh.style.width = (window.innerWidth - 80).toString() + "px";
+
+    errorMessageMain.style.width = window.innerWidth.toString() + "px";
+    errorMessageMain.style.height = window.innerHeight.toString() + "px";
+    errorMessageBody.style.top = ((window.innerHeight - 200 + 44) / 2).toString() + "px";
+    errorMessageBody.style.left = ((window.innerWidth - 200) / 2).toString() + "px";
 }
 
 function btnAddNewEntry_onmousedown() {
@@ -40,15 +47,106 @@ function btnBack_onmousedown() {
     btnBack.style.visibility = "hidden";
 }
 
+function tryParseDate(dateString) {
+    birthDateParts = dateString.split("/");
+    if (birthDateParts.length != 3) {
+        return false;
+    }
+    if (checkIfStringIsNumber(birthDateParts[0]) === false
+        || checkIfStringIsNumber(birthDateParts[1]) === false
+        || checkIfStringIsNumber(birthDateParts[2]) === false) {
+        return false;
+    }
+
+    var year = parseInt(birthDateParts[0]);
+    var month = parseInt(birthDateParts[1]);
+    var day = parseInt(birthDateParts[2]);
+
+    if (year < 1 || year > 9999
+        || month < 1 || month > 12
+        || day < 1 || day > 31) {
+        return false;
+    }
+
+    // check leap year
+
+    var isLeapYear = false;
+    if (year % 4 === 0
+        && !(year % 100 === 0 && year % 400 != 0)) {
+        isLeapYear = true;
+    }
+    if (isLeapYear === false && month === 2 && day > 28) {
+        return false;
+    }
+    if (isLeapYear === true && month === 2 && day > 29) {
+        return false;
+    }
+
+    if ((month === 1
+        || month === 3
+        || month === 5
+        || month === 7
+        || month === 8
+        || month === 10
+        || month === 12) && day > 31) {
+        return false;
+    }
+    else if (day > 30) {
+        return false;
+    }
+
+    return true;
+}
 
 function btnRefresh_onmousedown() {
-
+    if (validateInput() === false) {
+        return;
+    }
 }
 
 function validateInput() {
+    var beginDateErrorMessage = "Enter a valid begin date";
+    var endDateErrorMessage = "Enter a valid end date that is no later than today's date";
 
+    if (tryParseDate(txtBeginDate.value.trim()) === false) {
+        showErrorMessage(beginDateErrorMessage, txtBeginDate);
+        return false;
+    }
+
+    if (tryParseDate(txtEndDate.value.trim()) === false) {
+        showErrorMessage(endDateErrorMessage, txtEndDate);
+        return false;
+    }
 }
 
-function showErrorMessage() {
+function showErrorMessage(message, objectToFocus) {
+    errorMessageMain.style.visibility = 'visible';
+    inputInformation.style.pointerEvents = 'none';
+    errorMessageString.innerHTML = message;
 
+    errorMessageMain.ObjectToFocus = objectToFocus;
+}
+
+function btnErrorMessageOK_onmousedown() {
+    errorMessageMain.style.visibility = 'hidden';
+    inputInformation.style.pointerEvents = 'all';
+    window.setTimeout(function () {
+        errorMessageMain.ObjectToFocus.focus();
+    }, 0);
+}
+
+
+function checkIfStringIsNumber(numberString) {
+    var checkDigits = numberString.split("");
+    if (checkDigits.length === 0) {
+        return false;
+    }
+    var checkNumbers = new RegExp('[0-9]');
+
+    for (var i = 0; i < checkDigits.length; i++) {
+        if (checkNumbers.test(checkDigits[i]) === false) {
+            return false;
+        }
+    }
+    return true;
 }
