73a75a3054cfbf2ee3b82bb6225c85caa358854f
patnir <rahul20patni@gmail.com>
2016-07-06 13:23:08 -0400

    Making a new server class with code

---
+++ Server.js
@@ -0,0 +1,39 @@
+﻿var gServerRoot = "http://localhost:50912/Default.aspx?";
+
+var gServerErrorMsg;
+
+function httpPost(url, reqString, callbackFunction) {
+    gServerErrorMsg = "";
+    var httpRequest = new XMLHttpRequest();
+    var aborted = false;
+
+    var timeoutFunction = setTimeout(httpPostAsyncTimeout, 30000);
+
+    httpRequest.onreadystatechange = httpStateChange;
+    httpRequest.open("POST", url, true);
+    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    httpRequest.send(reqString);
+
+    function httpPostAsyncTimeout() {
+        aborted = true;
+        httpRequest.abort();
+        gServerErrorMsg = "Timeout connecting with the server";
+    }
+
+    function httpStateChange() {
+        if (httpRequest.readyState === 4) {
+            clearTimeout(timeoutFunction);
+            if (httpRequest.status === 200) {
+                if (httpRequest.responseText.substring(0, 6) === "error\n") {
+                    gServerErrorMsg = httpRequest.responseText.substring(6);
+                }
+                callbackFunction(httpRequest.responseText);
+            }
+            else {
+                if (aborted === false) {
+                    gServerErrorMsg = "Error received from the server.";
+                }
+            }
+        }
+    }
+}
