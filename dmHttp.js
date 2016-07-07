2ad415557842261f58999351ae24b2563c6bfb66
patnir <rahul20patni@gmail.com>
2016-07-07 17:10:12 -0400

    Change to request string

--- dmHttp.js
+++ dmHttp.js
@@ -1,40 +1,40 @@
-﻿var gServerRoot = "http://900f0398.ngrok.io/Default.aspx?";
+﻿//var gServerRoot = "http://2af2dda9.ngrok.io/Default.aspx?";
 
-//Example function call: httpPost(gServerRoot + "action=getEntries", requestString, function (responseString) { alert(responseString); } );
+////Example function call: httpPost(gServerRoot + "action=getEntries", requestString, function (responseString) { alert(responseString); } );
 
-function httpPost(url, reqString, callbackFunction) {
+//function httpPost(url, reqString, callbackFunction) {
 
-    var httpRequest = new XMLHttpRequest();
-    var aborted = false;
+//    var httpRequest = new XMLHttpRequest();
+//    var aborted = false;
 
-    var timeoutFunction = setTimeout(httpPostAsyncTimeout, 30000);
+//    var timeoutFunction = setTimeout(httpPostAsyncTimeout, 30000);
 
-    httpRequest.onreadystatechange = httpStateChange;
-    httpRequest.open("POST", url, true);
-    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    httpRequest.send(reqString);
+//    httpRequest.onreadystatechange = httpStateChange;
+//    httpRequest.open("POST", url, true);
+//    httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+//    httpRequest.send(reqString);
 
-    function httpPostAsyncTimeout() {
-        aborted = true;
-        httpRequest.abort();
-        alert("Timeout connecting with the server");
-    }
+//    function httpPostAsyncTimeout() {
+//        aborted = true;
+//        httpRequest.abort();
+//        alert("Timeout connecting with the server");
+//    }
 
-    function httpStateChange() {
-        if (httpRequest.readyState === 4) {
-            clearTimeout(timeoutFunction);
-            if (httpRequest.status === 200) {
-                if (httpRequest.responseText.substring(0, 6) === "error\n") {
-                    alert("Error returned from server: " + httpRequest.responseText.substring(6));
-                }
-                callbackFunction(httpRequest.responseText);
-            }
-            else {
-                if (aborted === false) {
-                    alert(httpRequest.responseText);
-                    alert("Error received from the server.");
-                }
-            }
-        }
-    }
-}
+//    function httpStateChange() {
+//        if (httpRequest.readyState === 4) {
+//            clearTimeout(timeoutFunction);
+//            if (httpRequest.status === 200) {
+//                if (httpRequest.responseText.substring(0, 6) === "error\n") {
+//                    alert("Error returned from server: " + httpRequest.responseText.substring(6));
+//                }
+//                callbackFunction(httpRequest.responseText);
+//            }
+//            else {
+//                if (aborted === false) {
+//                    alert(httpRequest.responseText);
+//                    alert("Error received from the server.");
+//                }
+//            }
+//        }
+//    }
+//}
