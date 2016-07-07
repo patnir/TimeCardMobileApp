2f97df05acd03412e830c4b38bb8c4722b6a98c3
patnir <rahul20patni@gmail.com>
2016-07-07 17:10:29 -0400

    Changing to sychronous

--- Server.js
+++ Server.js
@@ -1,39 +1,119 @@
-﻿var gServerRoot = "http://900f0398.ngrok.io/Default.aspx?";
-
+﻿/// <reference path="TimeCardv10.html">
+var gServerRoot = "http://localhost:50912/Default.aspx?";
 var gServerErrorMsg;
 
-function httpPost(url, reqString, callbackFunction) {
+function SignIn(teamName, emailAddress, password) {
+    var userCredentials = {
+        TeamName: teamName,
+        EmailAddress: emailAddress,
+        Password: password
+    }
+
+    var requestString = JSON.stringify(userCredentials)
+    var responseString = httpPost(gServerRoot + "action=signIn", requestString);
+
+    if (gServerErrorMsg !== "") {
+        return null;
+    }
+
+    var user = JSON.parse(responseString);
+    return user;
+}
+
+function tokenSignIn(authToken) {
+    var responseString = httpPost(gServerRoot + "action=tokenSignIn&authToken=" + authToken, "");
+
+    if (gServerErrorMsg !== "") {
+        return;
+    }
+
+    var user = JSON.parse(responseString);
+    return user;
+}
+
+function getEntries(userID, projectTitle, taskTitle, activityTitle, hoursWorkedLow,
+   hoursWorkedHigh, fromDate, toDate, entryDescription, billable, payable, inactive, authToken) {
+
+    var entriesList = [];
+    var timeLogCredentials = {
+        UserID: userID,
+        //ProjectTitle: projectTitle,
+        //TaskTitle: taskTitle,
+        //HoursWorkedLow: hoursWorkedLow,
+        //HoursWorkedHigh: hoursWorkedHigh,
+        FromDate: fromDate,
+        ToDate: toDate
+        //EntryDescription: entryDescription,
+        //ActivityTitle: activityTitle,
+        //BillableIndicator: billable,
+        //PayableIndicator: payable,
+        //IncludeInactiveProjects: inactive,
+    };
+
+    requestString = JSON.stringify(timeLogCredentials);
+    var responseString = httpPost(gServerRoot + "action=getEntries&authToken=" + authToken, requestString);
+
+    if (gServerErrorMsg !== "") {
+        return;
+    }
+
+    var entries = JSON.parse(responseString);
+
+    for (var i = 0; i < entries.length; i++) {
+        var entry = new TimeLogEntry();
+        entry.Deserialize(entries[i]);
+        entriesList.push(entry);
+    }
+
+    return entriesList;
+}
+
+function getProjects(authToken) {
+    var responseString = httpPost(gServerRoot + "action=getProjects&authToken=" + authToken);
+
+    if (gServerErrorMsg !== "") {
+        return null;
+    }
+
+    var projects = JSON.parse(responseString);
+    return projects;
+}
+
+function getTasks(authToken) {
+    var responseString = httpPost(gServerRoot + "action=getTasks&authToken=" + authToken);
+
+    if (gServerErrorMsg !== "") {
+        return null;
+    }
+
+    var tasks = JSON.parse(responseString);
+    return tasks;
+}
+
+function getActivities(authToken) {
+    var responseString = httpPost(gServerRoot + "action=getActivities&authToken=" + authToken);
+
+    if (gServerErrorMsg !== "") {
+        return null;
+    }
+
+    var activities = JSON.parse(responseString);
+    return activities;
+}
+
+function httpPost(url, reqString) {
     gServerErrorMsg = "";
     var httpRequest = new XMLHttpRequest();
-    var aborted = false;
-
-    var timeoutFunction = setTimeout(httpPostAsyncTimeout, 30000);
-
-    httpRequest.onreadystatechange = httpStateChange;
-    httpRequest.open("POST", url, true);
+    httpRequest.open("POST", url, false);
     httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     httpRequest.send(reqString);
 
-    function httpPostAsyncTimeout() {
-        aborted = true;
-        httpRequest.abort();
-        gServerErrorMsg = "Timeout connecting with the server";
+    var responseParts = httpRequest.responseText.split("\n");
+
+    if (responseParts[0] === "error") {
+        gServerErrorMsg = responseParts[1];
+        return;
     }
 
-    function httpStateChange() {
-        if (httpRequest.readyState === 4) {
-            clearTimeout(timeoutFunction);
-            if (httpRequest.status === 200) {
-                if (httpRequest.responseText.substring(0, 6) === "error\n") {
-                    gServerErrorMsg = httpRequest.responseText.substring(6);
-                }
-                callbackFunction(httpRequest.responseText);
-            }
-            else {
-                if (aborted === false) {
-                    gServerErrorMsg = "Error received from the server.";
-                }
-            }
-        }
-    }
+    return responseParts[1];
 }
